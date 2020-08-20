3577646ade0673ea725e0a0087a31f94317f595e
김동현 <gimdonghyeon@gimdonghyeon-ui-MacBookPro.local>
2020-08-21 00:03:15 +0900

    add: navi title, bar button

--- YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Dong/MenuListVC.swift
+++ YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Dong/MenuListVC.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class MenuListVC: UIViewController {
     
+    // MARK: Properties
+    
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "임시 next", style: .plain, target: self, action: #selector(didTapNext))
+        configure()
+        configureViews()
     }
     
-    // MARK: @objc
-    @objc private func didTapNext() {
-        let detailMenuVC = DetailMenuVC()
-        detailMenuVC.modalPresentationStyle = .fullScreen
-        self.present(detailMenuVC, animated: true)
+    // MARK: @Objc
+    @objc private func didTapSearchButton() {
+        
     }
+    
+    @objc private func didTapBackButton() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: Configure
+    private func configure() {
+        
+    }
+    
+    // MARK: ConfigureViews
+    private func configureViews() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .black
+        title = "가게 명"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(didTapSearchButton))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action: #selector(didTapBackButton))
+    }
+    
 }
+
+
+
+//navigationItem.rightBarButtonItem = UIBarButtonItem(title: "임시 next", style: .plain, target: self, action: #selector(didTapNext))
+//// MARK: @objc
+//@objc private func didTapNext() {
+//    let detailMenuVC = DetailMenuVC()
+//    detailMenuVC.modalPresentationStyle = .fullScreen
+//    self.present(detailMenuVC, animated: true)
+//}
