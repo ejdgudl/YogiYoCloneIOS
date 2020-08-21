4cb671ea302ee95168f1569533b3382f514dad09
김동현 <gimdonghyeon@gimdonghyeon-ui-MacBookPro.local>
2020-08-21 22:54:53 +0900

    feat: notiView 추가

--- YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Dong/CollectionViewCell/StoreInfoCell.swift
+++ YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Dong/CollectionViewCell/StoreInfoCell.swift
@@ -47,8 +47,50 @@ class StoreInfoCell: UICollectionViewCell {
         stackView.axis = .vertical
         stackView.spacing = 2
         stackView.distribution = .equalSpacing
-//        stackView.alignment = .leading
         return stackView
+    }()
+    
+    private let notiTitle: UILabel = {
+        let label = UILabel()
+        label.text = "사장님알림"
+        label.font = UIFont.systemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let moreButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("더보기 >", for: . normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        return button
+    }()
+    
+    private var notiTextLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.numberOfLines = 2
+        label.text = "aslkdfl;kasdf;akjsdhf;lashdf;lasadfasdfalsdfhalsjkdflasdflkasjdflkasdjflkjsdhf;lahsjdf;lkajsd;fiansugafnsjkglnafslgna;slkfgn;alskdjfg;laksdjf;lasdjf;laisdjflijsdjfli"
+        return label
+    }()
+    
+    private lazy var notiView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        [notiTitle, moreButton, notiTextLabel].forEach {
+            view.addSubview($0)
+        }
+        notiTitle.snp.makeConstraints { (make) in
+            make.top.left.equalToSuperview().inset(11)
+        }
+        moreButton.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().inset(5)
+            make.right.equalToSuperview().inset(11)
+        }
+        notiTextLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(self.notiTitle.snp.bottom).offset(12)
+            make.left.right.equalToSuperview().inset(11)
+            make.bottom.equalToSuperview().inset(12)
+        }
+        return view
     }()
     
     // MARK: Init
@@ -71,13 +113,18 @@ class StoreInfoCell: UICollectionViewCell {
     private func configureViews() {
         backgroundColor = .white
         
-        [payStackView].forEach {
+        [payStackView, notiView].forEach {
             addSubview($0)
         }
         
         payStackView.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().inset(2)
-            make.left.equalToSuperview().inset(2)
+            make.top.equalToSuperview()
+            make.left.equalToSuperview()
+        }
+        
+        notiView.snp.makeConstraints { (make) in
+            make.top.equalTo(self.payStackView.snp.bottom).offset(12)
+            make.left.right.equalToSuperview()
         }
     }
 }
