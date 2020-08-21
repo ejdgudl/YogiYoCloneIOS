b8b563f05d4010079c7469fa1cdd27c64379c27d
Qussk <by1106007@gmail.com>
2020-08-21 17:05:44 +0900

    모델추가(Color,Font)

--- YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Byun/DetailMenuVC.swift
+++ YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Byun/DetailMenuVC.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import Alamofire
 
 class DetailMenuVC: UIViewController {
+  
+  let testsLable : UILabel = {
+    let l = UILabel(frame: CGRect(x: 0, y: 0, width: 250, height: 90))
+  //  l.font = UIFont(name: FontModel.customSemibold, size: 20)
+    l.font = FontModel.toSize.customFont
+    l.numberOfLines = 2
+    l.text = "전화주문"//"피자알볼로(POSMENU FINAL)"
+    l.textColor = .black//ColorPiker.customDarkGray
+    return l
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .lightGray
-        
-    }
+   // Alamofire()
+    
+    testsLable.center = self.view.center
+    view.addSubview(testsLable)
+  }
+  
+  
 }
+
