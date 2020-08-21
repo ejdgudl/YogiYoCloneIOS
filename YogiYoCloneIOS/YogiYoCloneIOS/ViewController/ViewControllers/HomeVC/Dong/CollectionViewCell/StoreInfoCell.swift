9494a7488de196b764757d68630c18ff26f08704
김동현 <gimdonghyeon@gimdonghyeon-ui-MacBookPro.local>
2020-08-21 22:06:12 +0900

    style: label의 attribute 설정 코드 재사용으로 인한 코드량 증가를 method 생성후 uilabel extension에 추가하여 대응

--- YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Dong/CollectionViewCell/StoreInfoCell.swift
+++ YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Dong/CollectionViewCell/StoreInfoCell.swift
@@ -22,32 +22,23 @@ class StoreInfoCell: UICollectionViewCell {
     private lazy var minOrderPrice: UILabel = {
         let label = UILabel()
         let price = numberFormatter.string(from: NSNumber(value: minPrice))
-        let string = "최소주문  " + "\(price ?? "")" + "원"
-        label.font = UIFont.systemFont(ofSize: 11, weight: .regular)
-        let attributedString = NSMutableAttributedString(string: string)
-        attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 12, weight: .semibold), range: NSRange(location: 0, length: 4))
-        label.attributedText = attributedString
+        let string = "·최소주문  " + "\(price ?? "")" + "원"
+        label.setAttribute(label: label, string: string)
         return label
     }()
     
     private lazy var payMentLbl: UILabel = {
         let label = UILabel()
-        let string = "결제방법  \(payMent)"
-        label.font = UIFont.systemFont(ofSize: 11, weight: .regular)
-        let attributedString = NSMutableAttributedString(string: string)
-        attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 12, weight: .semibold), range: NSRange(location: 0, length: 4))
-        label.attributedText = attributedString
+        let string = "·결제방법  \(payMent)"
+        label.setAttribute(label: label, string: string)
         return label
     }()
     
     private lazy var deliveryPrice: UILabel = {
         let label = UILabel()
         let price = numberFormatter.string(from: NSNumber(value: deliPrice))
-        let string = "배달요금  " + "\(price ?? "")" + "원"
-        label.font = UIFont.systemFont(ofSize: 11, weight: .regular)
-        let attributedString = NSMutableAttributedString(string: string)
-        attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 12, weight: .semibold), range: NSRange(location: 0, length: 4))
-        label.attributedText = attributedString
+        let string = "·배달요금  " + "\(price ?? "")" + "원"
+        label.setAttribute(label: label, string: string)
         return label
     }()
     
