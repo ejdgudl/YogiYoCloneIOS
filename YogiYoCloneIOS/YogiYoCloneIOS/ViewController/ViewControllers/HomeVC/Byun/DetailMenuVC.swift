1c02e88ca564355bd5d700477d66ac613d72a238
Qussk <by1106007@gmail.com>
2020-08-21 20:31:24 +0900

    View start

--- YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Byun/DetailMenuVC.swift
+++ YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Byun/DetailMenuVC.swift
@@ -7,28 +7,31 @@
 //
 
 import UIKit
-import Alamofire
 
 class DetailMenuVC: UIViewController {
+// test용
+//  let testsLable : UILabel = {
+//    let l = UILabel(frame: CGRect(x: 0, y: 0, width: 250, height: 90))
+//  //  l.font = UIFont(name: FontModel.customSemibold, size: 20)
+//    l.font = FontModel.toSize.customFont
+//    l.numberOfLines = 2
+//    l.text = "전화주문"//"피자알볼로(POSMENU FINAL)"
+//    l.textColor = .black//ColorPiker.customDarkGray
+//    return l
+//  }()
   
-  let testsLable : UILabel = {
-    let l = UILabel(frame: CGRect(x: 0, y: 0, width: 250, height: 90))
-  //  l.font = UIFont(name: FontModel.customSemibold, size: 20)
-    l.font = FontModel.toSize.customFont
-    l.numberOfLines = 2
-    l.text = "전화주문"//"피자알볼로(POSMENU FINAL)"
-    l.textColor = .black//ColorPiker.customDarkGray
-    return l
-  }()
+  let tableView = UITableView()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
+ //   testsLable.center = self.view.center
+ //   view.addSubview(testsLable)
     
-   // Alamofire()
-    
-    testsLable.center = self.view.center
-    view.addSubview(testsLable)
+  //  tableView.backgroundView = .red
+    tableView.frame = UIScreen().bounds
+  //  tableView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellReuseIdentifier: <#T##String#>)
+    view.addSubview(tableView)
   }
   
   
