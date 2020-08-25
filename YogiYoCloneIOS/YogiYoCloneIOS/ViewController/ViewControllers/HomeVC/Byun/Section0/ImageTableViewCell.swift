b3cd260d84f4f3ae031aba5a5b055e055f7b7e9b
Qussk <by1106007@gmail.com>
2020-08-25 15:18:04 +0900

    새로받기

---
+++ YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Byun/Section0/ImageTableViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  ImageTableViewCell.swift
+//  YogiYoCloneIOS
+//
+//  Created by Qussk_MAC on 2020/08/25.
+//  Copyright © 2020 김동현. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+  
+  let detailmenuImage = UIImageView()
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    
+    setUI()
+    constrian()
+  }
+  
+  func setUI(){
+    selectionStyle = .none
+    layer.masksToBounds = true
+    setImage()
+    
+  }
+  
+  func setImage(){
+    //detailmenuImage.layer.masksToBounds = true
+    detailmenuImage.backgroundColor = .black
+    detailmenuImage.contentMode = .scaleAspectFit
+    detailmenuImage.contentMode = .center
+    detailmenuImage.image = UIImage(named: "TopImageView")
+    detailmenuImage.layer.masksToBounds = true
+    
+    contentView.addSubview(detailmenuImage)
+    
+    
+  }
+  
+  
+  func constrian() {
+    [detailmenuImage].forEach{
+      contentView.addSubview($0)
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      
+    }
+    //Full
+    NSLayoutConstraint.activate([
+      detailmenuImage.topAnchor.constraint(equalTo: contentView.topAnchor),
+      detailmenuImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+      detailmenuImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+      detailmenuImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+    ])
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func configure(){
+    //셀데이터 넣을 곳
+  }
+}
---
+++ YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Byun/Section2/MenuTableViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  MenuTableViewCell.swift
+//  YogiYoCloneIOS
+//
+//  Created by Qussk_MAC on 2020/08/25.
+//  Copyright © 2020 김동현. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+  
+  let menuLable: UILabel = {
+    let l = UILabel()
+    l.font = FontModel.toSize.customFont
+    l.text = "추가 / 변경 선택"
+    l.textColor = .black //ColorPiker.customDarkGray
+    return l
+  }()
+  
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    
+    contentView.addSubview(menuLable)
+    setConstrain()
+  }
+  
+  
+  func setConstrain(){
+    menuLable.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      menuLable.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+      menuLable.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+      menuLable.heightAnchor.constraint(equalToConstant: 30),
+    ])
+  }
+  
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+}
