b2d152604fcd962464197d3bbb47802eae8ab530
SROO0524 <vmflstm847@naver.com>
2020-08-27 00:05:17 +0900

    StoreList TableView Setting

--- YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Siroo/ViewController/StoreListCell.swift
+++ YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Siroo/ViewController/StoreListCell.swift
@@ -55,7 +55,7 @@ class StoreListCell: UITableViewCell {
         let label = UILabel()
         label.text = "불고기 와퍼 , 트러플 머쉬룸 와퍼"
         label.font = FontModel.toSize.customSmallFont
-        label.textColor = ColorPiker.customrightGray
+        label.textColor = .black
 
         return label
     }()
@@ -64,7 +64,7 @@ class StoreListCell: UITableViewCell {
        let label = UILabel()
         label.text = " 30~40분"
         label.font = FontModel.toSize.customSmallFont
-        label.textColor = ColorPiker.customDarkGray
+        label.textColor = .black
 
         return label
     }()
@@ -91,6 +91,7 @@ class StoreListCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         selectionStyle = .none
+        setUIConstraints()
         
         
         
@@ -104,23 +105,53 @@ class StoreListCell: UITableViewCell {
 //    MARK: SetUIConstraints
     
     func setUIConstraints () {
-        [storeImage,storeNameLabel,starImage,storeRateLabel,reviewLabel,bestMenuLabel,estimatedTime].forEach({
+        [storeImage,storeNameLabel,bestMenuLabel,estimatedTime].forEach({
             self.addSubview($0)
         })
         
         storeImage.snp.makeConstraints { (make) in
-            make.top.leading.equalToSuperview().offset(10)
-            make.trailing.equalToSuperview().offset(-150)
-            make.bottom.equalToSuperview().offset(-10)
+            make.top.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-300)
+            make.bottom.equalToSuperview().offset(-20)
         }
         
         storeNameLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(storeImage)
-            make.leading.equalTo(storeImage).offset(10)
+            make.top.equalToSuperview().offset(5)
+            make.leading.equalTo(storeImage.snp.trailing).offset(20)
             make.trailing.equalToSuperview().offset(-100)
-            make.bottom.equalToSuperview().offset(-100)
+            make.bottom.equalToSuperview().offset(-40)
+        }
+            
+        let stack = UIStackView(arrangedSubviews: [starImage,storeRateLabel,reviewLabel])
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.spacing = 4
+        
+        addSubview(stack)
+        stack.snp.makeConstraints { (make) in
+            make.top.equalTo(storeNameLabel.snp.bottom).offset(5)
+            make.leading.equalTo(storeNameLabel.snp.leading)
+            make.trailing.equalToSuperview().offset(-100)
+            make.bottom.equalToSuperview().offset(-35)
         }
         
+        
+        
+        bestMenuLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(stack.snp.bottom).offset(5)
+            make.leading.equalTo(stack.snp.leading)
+            make.trailing.equalToSuperview().offset(-50)
+            make.bottom.equalToSuperview().offset(-15)
+        }
+        
+        estimatedTime.snp.makeConstraints { (make) in
+            make.top.equalTo(bestMenuLabel.snp.top)
+            make.leading.equalTo(bestMenuLabel.snp.trailing).offset(50)
+            make.trailing.equalToSuperview().offset(-20)
+            make.bottom.equalTo(bestMenuLabel.snp.bottom)
+        }
+
+
         
     }
     
