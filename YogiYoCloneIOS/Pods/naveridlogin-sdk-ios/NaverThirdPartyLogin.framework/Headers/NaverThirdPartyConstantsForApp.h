4f3fcf120419c4c886895d98d8e82758b60e9b53
Qussk <by1106007@gmail.com>
2020-09-14 20:16:16 +0900

    feat: test6

--- YogiYoCloneIOS/Pods/naveridlogin-sdk-ios/NaverThirdPartyLogin.framework/Headers/NaverThirdPartyConstantsForApp.h
+++ YogiYoCloneIOS/Pods/naveridlogin-sdk-ios/NaverThirdPartyLogin.framework/Headers/NaverThirdPartyConstantsForApp.h
@@ -32,8 +32,8 @@ typedef enum {
     OK
 } OAuthLoginState;
 
-#define kServiceAppUrlScheme    @"naverlogin"
+#define kServiceAppUrlScheme    @"thirdparty20samplegame"
 
-#define kConsumerKey            @"FSjDRGSStCHJtAFLpslT"
-#define kConsumerSecret         @"lkNivk3ZC6"
-#define kServiceAppName         @"YogiYoCloneIOS"
+#define kConsumerKey            @"jyvqXeaVOVmV"
+#define kConsumerSecret         @"527300A0_COq1_XV33cf"
+#define kServiceAppName         @"네이버 아이디로 로그인"
