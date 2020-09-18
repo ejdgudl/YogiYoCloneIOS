cc6c53df285fc6b774fe7e7b35418cc3fe7a9807
SROO0524 <vmflstm847@naver.com>
2020-09-18 12:44:23 +0900

    Data Model & StoreVC : category Data Edit

--- YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Siroo/ViewController/StoreListVC.swift
+++ YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Siroo/ViewController/StoreListVC.swift
@@ -23,27 +23,25 @@ public let scrollView = UIScrollView()
 class StoreListVC: UIViewController, CustomTopCategoryViewDelegate, RestaurantModelProtocol , UIScrollViewDelegate , categoryVCdelegate {
     
     
-//    MARK: Properties
+    //    MARK: Properties
     
     private let storeListCell = StoreListCell()
     private let menuList = MenuListVC()
     private let scrollView = UIScrollView()
-    private let storeFilterView = StoreListFilterView()
+    public let storeFilterbigView = StoreFilterbigView()
     public var categoryIndex : Int = 0
     
     private var scrollViewIndex : Int  = 0
-
-    private let first = CategoryVC()
-    private let second = CategoryVC()
-    private let third = CategoryVC()
-    private let fourth = CategoryVC()
-    private let fifth = CategoryVC()
     
-    private let categoryVC : [UIViewController] = []
     
     private let fetchModel = StoreinfoFetch()
     
     private var codeSegmented: CustomTopCategoryView?
+    
+    private let categories: [String] = StoreinfoFetch.categories
+    //    private let categoriesVC: [CategoryVC] = [CategoryVC](repeating: CategoryVC(), count: StoreinfoFetch.categories.count)
+    
+    private let categoriesVC: [CategoryVC] = [CategoryVC(), CategoryVC(), CategoryVC(), CategoryVC(), CategoryVC()]
     
     weak var delegate: StoreListVCDelegate?
     
@@ -79,43 +77,33 @@ class StoreListVC: UIViewController, CustomTopCategoryViewDelegate, RestaurantMo
         labal.font = UIFont(name: FontModel.customSemibold, size: 18)
         return labal
     }()
-
-//    MARK: LifeCycle
+    
+    //    MARK: LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = ColorPiker.customSystem
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "임시 next", style: .plain, target: self, action: #selector(didTapNext))
         
         fetchModel.delegate = self
         configure()
+        fetchModel.fetchAll()
         configureScrollView()
         configureFilterButton()
-        view.addSubview(storeFilterView)
-        
-        self.storeFilterView.snp.makeConstraints { (make) in
-            make.top.bottom.equalTo(800)
-            make.leading.trailing.equalToSuperview()
-        }
         
         categoryButtonScrollAction(to: categoryIndex)
         codeSegmented?.indexChangedListener(categoryIndex)
-
-        first.categoryDelegate = self
-        second.categoryDelegate = self
-        third.categoryDelegate = self
-        fourth.categoryDelegate = self
-        fifth.categoryDelegate = self
         
-        
+        for (_, vc) in categoriesVC.enumerated() {
+            vc.categoryDelegate = self
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-//    MARK:  Selector
+    //    MARK:  Selector
     @objc private func didTapNext() {
         let menuListVC = MenuListVC()
         navigationController?.pushViewController(menuListVC, animated: true)
@@ -123,57 +111,47 @@ class StoreListVC: UIViewController, CustomTopCategoryViewDelegate, RestaurantMo
     
     
     @objc func filterButtonTap(_ sender: UIButton) {
-        UIView.animate(withDuration: 2, delay: 0, options: [.curveEaseIn],
-                               animations: {
-                                self.storeFilterView.center.y -= 500
-//                                self.storeFilterView.layoutIfNeeded()
-                }, completion: nil)
-//                self.storeFilterView.isHidden = false
-        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tabCloseFilter))
+        storeFilterbigView.addGestureRecognizer(tap)
+        view.addSubview(storeFilterbigView)
+        self.storeFilterbigView.snp.makeConstraints { (make) in
+            make.top.bottom.equalTo(0)
+            make.leading.trailing.equalToSuperview()
+        }
+        storeFilterbigView.showMenu()
     }
-
-//self.storeFilterView.snp.makeConstraints { (make) in
-//    make.top.bottom.equalTo(0)
-//    make.leading.trailing.equalToSuperview()
-
+    
+    @objc func tabCloseFilter() {
+        storeFilterbigView.removeFromSuperview()
+    }
+    
+    
     
     // Button Gesture 했을때 색상하는 Selector
     @objc func filterButtoncolorChange() {
         filterbutton.backgroundColor = .systemGray
     }
     
-    
-    
-//    MARK: fetch event
-    func restaurantRetrived(restaurants: [AllListData.Results]) {
-        first.restaurants = restaurants
-        first.reload()
-        second.restaurants = restaurants
-        second.reload()
-        third.restaurants = restaurants
-        third.reload()
-        fourth.restaurants = restaurants
-        fourth.reload()
-        fifth.restaurants = restaurants
-        fifth.reload()
-
+    //    MARK: fetch event
+    func restaurantRetrived(restaurants: [AllListData.Results], index: Int) {
+        categoriesVC[index].restaurants = restaurants
+        categoriesVC[index].reload()
     }
-//    MARK: Configure
+    
+    //    MARK: Configure
     func configure() {
-        codeSegmented = CustomTopCategoryView(frame: CGRect(x: 0, y: 80, width: view.frame.width, height: 50), categoryTitles: ["전체보기","중국집","치킨","한식","피자/양식"])
+        codeSegmented = CustomTopCategoryView(frame: CGRect(x: 0, y: 80, width: view.frame.width, height: 50), categoryTitles: categories)
         codeSegmented?.backgroundColor = .white
         codeSegmented?.delegate = self
         view.addSubview(codeSegmented!)
         self.configureTableView()
-
+        
     }
     
     func configureTableView() {
-        self.scrollView.addSubview(self.first.configureTableView(index: 0))
-        self.scrollView.addSubview(self.second.configureTableView(index: 1))
-        self.scrollView.addSubview(self.third.configureTableView(index: 2))
-        self.scrollView.addSubview(self.fourth.configureTableView(index: 3))
-        self.scrollView.addSubview(self.fifth.configureTableView(index: 4))
+        for (index, vc) in categoriesVC.enumerated() {
+            self.scrollView.addSubview(vc.configureTableView(index: index))
+        }
     }
     
     @objc func testButtonAction(sender: UIButton) {
@@ -189,11 +167,11 @@ class StoreListVC: UIViewController, CustomTopCategoryViewDelegate, RestaurantMo
             make.top.equalTo(view.safeAreaLayoutGuide).offset(60)
             make.leading.trailing.equalTo(view.safeAreaLayoutGuide).offset(0)
             make.bottom.equalTo(view.safeAreaLayoutGuide).offset(0)
-  
+            
         }
         
         scrollView.delegate = self
-         
+        
         scrollView.contentSize = CGSize(width: self.view.frame.width * 5, height: scrollView.frame.height)
         print("table 뷰의 \(scrollView.frame.height)")
         scrollView.isPagingEnabled = true
@@ -216,7 +194,7 @@ class StoreListVC: UIViewController, CustomTopCategoryViewDelegate, RestaurantMo
     
     
     
-//    MARK: Category에 따라 스크롤 이동 : category의 customView 를 delegate로 받음
+    //    MARK: Category에 따라 스크롤 이동 : category의 customView 를 delegate로 받음
     
     func categoryButtonScrollAction(to index: Int) {
         categoryIndex = index
@@ -236,7 +214,7 @@ class StoreListVC: UIViewController, CustomTopCategoryViewDelegate, RestaurantMo
             codeSegmented?.indexChangedListener(categoryIndex)
         }
     }
-//    MARK: Category delegate
+    //    MARK: Category delegate
     func categoryDelegate(id: Int) {
         let menuList = MenuListVC()
         menuList.id = id
@@ -244,13 +222,15 @@ class StoreListVC: UIViewController, CustomTopCategoryViewDelegate, RestaurantMo
         navigationController?.navigationBar.tintColor = .gray
     }
     
-    
 }
+
+
 
 extension StoreListVC : StoreListFilterViewDelegate {
     func presentStorefilterView() {
         print("d")
     }
+
     
     
 }
