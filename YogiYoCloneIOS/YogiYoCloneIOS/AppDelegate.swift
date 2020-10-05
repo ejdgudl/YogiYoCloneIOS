2a14b338baf48954889c6d5fd7f74c939ecab8d7
Qussk <by1106007@gmail.com>
2020-10-05 18:06:09 +0900

    feat: fech data() - id

--- YogiYoCloneIOS/YogiYoCloneIOS/AppDelegate.swift
+++ YogiYoCloneIOS/YogiYoCloneIOS/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        //        window?.rootViewController = UINavigationController(rootViewController: StoreListVC())
+  //              window?.rootViewController = UINavigationController(rootViewController: MainTabVC())
         window?.rootViewController = MainTabVC()
         window?.makeKeyAndVisible()
         return true
