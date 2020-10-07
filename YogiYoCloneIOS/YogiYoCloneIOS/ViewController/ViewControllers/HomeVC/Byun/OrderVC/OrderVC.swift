7ecbbd3b94667f9235e60585e3fb2d7bfbbff1c0
Qussk <by1106007@gmail.com>
2020-10-07 16:04:03 +0900

    feat: OrderVC - alert

--- YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Byun/OrderVC/OrderVC.swift
+++ YogiYoCloneIOS/YogiYoCloneIOS/ViewController/ViewControllers/HomeVC/Byun/OrderVC/OrderVC.swift
@@ -111,8 +111,7 @@ class OderVC : UIViewController {
   //결제하기
   @objc func paymentDidTapButton(_ sender : UIButton){
     onPostShowBible()
-    dismiss(animated: true)
-    
+    alertController()    
   }
   
   func buttonFrame(){
@@ -163,7 +162,21 @@ class OderVC : UIViewController {
     // print(totalPrice())
   }
   
+  func alertController(){
+  
+    let alert = UIAlertController(title: "알림", message: "⛳️주문이 완료되었습니다~!🏇🚣‍♂️~!🧘‍♂️ 배달이 시작됩니다.🛥🚁", preferredStyle: UIAlertController.Style.alert)
+    alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: { action in
+      
+//      //2-1
+//           DatabaseManager.shared.insertUser(with: DatabaseManager.ChatAppUser(email: email, nickname: email, id: password))
+      
+     self.dismiss(animated: true)
+    }))
+    self.present(alert, animated: true, completion: nil)
+  }
 }
+
+
 //MARK:-UITableViewDataSource
 extension OderVC : UITableViewDataSource{
   func numberOfSections(in tableView: UITableView) -> Int {
